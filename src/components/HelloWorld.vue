11e8426f42bc13a8ae4fd739dfc2826aa3e631a5
guoxing <2933575030@qq.com>
2022-04-11 22:37:46 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="main-container">
+    <div style="width: 100%">
+      <MarkdownPreview :markdown-content="text"/>
+    </div>
+    <div style="width: 100%;">
+<!--      <MdComment />-->
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import MarkdownPreview from "./MarkdownPreview.vue";
+import MdComment from "./MarkdownComment.vue";
+export default {
+  components: {MdComment, MarkdownPreview},
+  data() {
+    return {
+      text: `
+## title1
+> hello
+## title2
+> hello
+## title3
+> hello
+## title4
+> hello
+## title5
+> hello
+## title6
+> hello
+      `
+    }
+  }
+}
+</script>
+<style scoped>
+.main-container {
+  width: 100%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ src/components/MarkdownPreview.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="md-editor-preview">
+    <MdEditor
+        editorId="preview-editor"
+        v-model="markdownContent"
+        :previewOnly="true"
+        :previewTheme="'vuepress'"
+    />
+    <MdCatalog editorId="preview-editor" :scrollElement="scrollElement" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import MdEditor from 'md-editor-v3'
+import 'md-editor-v3/lib/style.css'
+
+const MdCatalog = MdEditor.Catalog
+
+export default defineComponent({
+  name: 'MarkdownPreview',
+  components: {
+    MdEditor,
+    MdCatalog
+  },
+  props: {
+    markdownContent: {
+      type: String,
+      default: 'no data',
+      required: true
+    }
+  },
+  data() {
+    return {
+      scrollElement: document.scrollingElement
+    }
+  }
+})
+</script>
+<style scoped>
+.md-editor-preview {
+  display: flex;
+  flex-direction: row;
+}
+</style>
